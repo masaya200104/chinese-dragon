5625ddcfdb97dedd0a137b9c756421adab121e1f
masaya200104 <shimatatematate@gmail.com>
2024-06-07 12:04:10 +0900

    a

--- account_delete_check.php
+++ account_delete_check.php
@@ -1,5 +1,5 @@
 <?php require 'header.php'; ?>
-<div align="center">アカウントを削除しますか？</div>
+<div align="center">アカウントを削除しますか？　</div>
 <?php
 echo '<div align="center"><button><a href="account_delete.php?delete_id=',$_GET['id'],'">はい</a></button>';
 echo '<button><a href="mypage.php">いいえ</a></button></div>';
